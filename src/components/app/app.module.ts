8393cfc7947c26cd40bd2804ab6d63099ccbf889
Freek <sartrys.freek@hotmail.com>
2022-07-31 14:58:43 +0200

    Bootstrap works

--- src/components/app/app.module.ts
+++ src/components/app/app.module.ts
@@ -4,8 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
 
-
+const routing = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   declarations: [
@@ -14,10 +20,10 @@ import { RouterModule, Routes } from '@angular/router';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-    ])
+    RouterModule.forRoot(routing),
+    BrowserAnimationsModule,
+    MatCardModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
