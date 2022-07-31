64c8a298e5a60810dd1d67214106a7575ef7bd72
Freek <sartrys.freek@hotmail.com>
2022-07-31 21:23:01 +0200

    Picture from base64 test works

--- src/components/app/home/home.component.ts
+++ src/components/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../models/recipe';
-
+import {ImageFruit} from '../../../assets/imagesEncoded';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +12,7 @@ export class HomeComponent implements OnInit {
   cards: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   recipe: Recipe = new Recipe(1, "fruitsalade", "f", ["peer", "banaan", "appel", "kiwi"], "Cut all the ingredïents \n put them all togethere in a bowl", true, 1,1, "Freek");
   recipes: Array<Recipe> = [this.recipe];
-
+  image: string = new ImageFruit().imageFruit;
   constructor() { }
 
   ngOnInit(): void {
