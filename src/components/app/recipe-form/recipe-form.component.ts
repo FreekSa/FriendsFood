b09fd148a2f7f47bab78fb7cf25b342aa93f54a1
Freek <sartrys.freek@hotmail.com>
2022-08-06 12:29:02 +0200

    changes option bold based on selection

--- src/components/app/recipe-form/recipe-form.component.ts
+++ src/components/app/recipe-form/recipe-form.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeFormComponent implements OnInit {
   ingredients: string[] = [];
-
   ingredient: string = "";
+  amountOfTime = "";
 
   constructor() { }
 
