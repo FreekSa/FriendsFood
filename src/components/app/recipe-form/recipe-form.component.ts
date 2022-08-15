2e0ff997829955e1f5b28dfcaede9aa88055b85c
Freek <sartrys.freek@hotmail.com>
2022-08-15 21:03:36 +0200

    button disabled if form is not valid

--- src/components/app/recipe-form/recipe-form.component.ts
+++ src/components/app/recipe-form/recipe-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, NgForm } from '@angular/forms';
 import { Recipe } from '../models/recipe';
 import * as uuid from "uuid";
-import {ImageFruit} from '../../../assets/imagesEncoded';
+import { ImageFruit } from '../../../assets/imagesEncoded';
 
 
 @Component({
@@ -15,6 +15,7 @@ export class RecipeFormComponent implements OnInit {
   ingredient: string = "";
   amountOfTime = "";
   submitted = false;
+  selectedPrepTime = "";
 
   recipe = new Recipe(null, null, null, this.ingredientslist, null, false, null, null, null);
   recipeForm: NgForm;
@@ -25,29 +26,29 @@ export class RecipeFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  addIngredient(ingredient: string): void{
+  addIngredient(ingredient: string): void {
     console.log(ingredient);
-    if(ingredient){
+    if (ingredient) {
       this.ingredientslist.push(ingredient);
       this.ingredient = "";
     }
   }
 
-  removeIngredient(ingredient: string): void{
-    if(ingredient){
+  removeIngredient(ingredient: string): void {
+    if (ingredient) {
       const index = this.ingredientslist.indexOf(ingredient);
       console.log(index);
-      if(index != -1){
+      if (index != -1) {
         this.ingredientslist.splice(index, 1);
       }
     }
   }
 
-  addRecipe(){
+  addRecipe() {
     console.log("voeg recept toe");
   }
 
-  onSubmit(recipeForm: NgForm){
+  onSubmit(recipeForm: NgForm) {
     this.value = recipeForm.value;
     this.recipe.Id = uuid.v4();
     this.recipe.Title = this.value.Title;
@@ -56,7 +57,7 @@ export class RecipeFormComponent implements OnInit {
     this.recipe.Ingredients = this.ingredientslist;
     this.recipe.Persons = this.value.Persons;
     this.recipe.Vegan = this.value.Vegan;
-    this.recipe.PrepTime = this.value.PrepTime;
+    this.recipe.PrepTime = this.selectedPrepTime;
     console.log(this.recipe);
   }
 }
